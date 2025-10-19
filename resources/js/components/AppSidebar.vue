09d1e2e33c6b0071b32eae41c7dbe329798b1b1d
Laurynas Miškinis <laurynasmiskinis1@gmail.com>
2025-10-19 17:19:42 +0300

    Naive UI, ziggy js setup and some form work

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -11,18 +11,23 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from '@/components/ui/sidebar';
-import { dashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, Folder, LayoutGrid, FileText } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
+import { route } from 'ziggy-js';
 
 const mainNavItems: NavItem[] = [
     {
         title: 'Dashboard',
-        href: dashboard(),
+        href: route('dashboard.index'),
         icon: LayoutGrid,
     },
+    {
+      title: 'Simple Form',
+      href: route('simple.form.index'),
+      icon: FileText,
+    }
 ];
 
 const footerNavItems: NavItem[] = [
@@ -45,7 +50,7 @@ const footerNavItems: NavItem[] = [
             <SidebarMenu>
                 <SidebarMenuItem>
                     <SidebarMenuButton size="lg" as-child>
-                        <Link :href="dashboard()">
+                        <Link :href="route('dashboard.index')">
                             <AppLogo />
                         </Link>
                     </SidebarMenuButton>
